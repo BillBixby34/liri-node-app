4f02d62b08a3d570ab8539f801c7952e359a2582
Will H <wj_hilliard@yahoo.com>
2017-08-01 03:10:34 -0400

    changes, but needs finishing

--- liri.js
+++ liri.js
@@ -16,7 +16,7 @@ console.log("");
 };
 //Intro();
 //begin values
-
+var fs = require("fs");
 var logKeys = require("./keys.js");
 var spotify = require('node-spotify-api');
 var twitter = require("twitter");
@@ -25,7 +25,7 @@ var request = require("request");
 //var nodeArg = process.argv;
 var speak = process.argv[2];
 var truthy = process.argv[3];
-//for (var i = 2; i < nodeArg.length; i++){
+//for (var i = 3; i < nodeArg.length; i++){
 //	address = address + " " + nodeArg[i];
 //}
 //request() {
@@ -65,11 +65,61 @@ switch (speak) {
 
 
 //functions//
-//function spotSearch(){};
-//function tweetSearch(){};
+function spotSearch(){
+	var spotter = new Spotify(theKeys.spotifyKeys);
+
+	var playSong = truthy;
+
+	//retrieving Spotify song
+
+	spotter.search({ type: 'track', query: playSong, limit: 1}, function(err, data) {
+
+  		if (err) {
+
+    		return console.log('Error occurred: ' + error);
+
+  			} 
+
+				console.log(data);			
+  
+		});
+};
+function tweetSearch(){
+	//needs refinement
+	var client = new Twitter(logKeys.twitterKeys);
+
+	var params = {screen_name: 'BondedWords',
+
+				limit: 20
+
+			};
+
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+
+	  if (!error) {
+
+	  	for (var i=0; i < tweets.length; i++){
+
+	  		console.log("\ntwtwtwtwtwtwtwtwtwtwtwtwtwtwtwtw")
+
+	  		console.log(" ");
+
+	  		console.log(tweets[i].created_at)
+
+			console.log(tweets[i].text);
+
+			console.log("\ntwtwtwtwtwtwtwtwtwtwtwtwtwtwtwtw")
+
+	   		}
+
+	   	}
+
+	});
+};
 function movieSearch() {
 
-	var queryURL = "http://www.omdbapi.com/?t=" + truthy + "&y=&plot=short&apikey=40e9cece";
+	var queryURL = "http://www.omdbapi.com/?t=" + truthy + "&y=&plot=short&tomatoes=true&apikey=40e9cece";
+	//wanted truthy to join, not working yet
 	request(queryURL, function (error, response, body) {
 
 		if (!error && response.statusCode === 200) {
@@ -97,9 +147,9 @@ function movieSearch() {
 			console.log("\nRotten Tomatoes rating is: " + JSON.parse(body).Ratings[2].Value);
 
 			console.log("\nOMOMOMOMOMOMOMOMOMOMOMOMOMOMOM");
-
+			//only able to pass 1 argument, RT stops also
 		}
 
 	});
-}
+};
 //function liriDo();
